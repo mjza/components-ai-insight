4cc8a60f18411515b037888bf0be735c3f870326
mahdi.ansari@ucalgary.ca <mahdi.ansari@ucalgary.ca>
2025-03-11 20:49:44 -0600

    Fix DB name

--- 09_update_bert_se_similarity.py
+++ 09_update_bert_se_similarity.py
@@ -15,7 +15,7 @@ load_dotenv()
 # Read database connection details from environment variables
 DB_USER = os.getenv("DB_USER")
 DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
+DB_NAME = os.getenv("DBC_NAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_PORT = os.getenv("DB_PORT")
 
